3e8dcd323e71de1894e770da23c6c518d21f1b0d
Philip Sterling <philipssterlings@gmail.com>
2019-06-02 01:13:53 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -58,7 +58,7 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
-  for counter in 1.number_of_times do
+  for counter in 1..number_of_times do
    puts phrase
  end
 end
